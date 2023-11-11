87d22f11d6961e12dd449c6644e2a92213cfa3d2
Conor Kavanagh <116828308+conkavanagh@users.noreply.github.com>
2023-11-11 17:57:01 +0000

    Time: 1 ms (100.00%), Space: 44.7 MB (84.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] newNums = new int[nums.length];
+        //int[] newNums = new int[nums.length];
         int index = 0;
         for(int i:nums)
         {
            if (i!=0)
            {
-               newNums[index] = i;
+               nums[index] = i;
                index++;
            }
         }
         for(int i = index; i<nums.length; i++)
         {
-            newNums[i] = 0;
-        }
-        for(int i = 0; i<nums.length; i++)
-        {
-            nums[i] = newNums[i];
+            nums[i] = 0;
         }
     }
 }
