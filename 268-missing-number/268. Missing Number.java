cee1f9bc5ce2308752e0513a6a7b0c0f26d8afe9
Conor Kavanagh <116828308+conkavanagh@users.noreply.github.com>
2024-03-07 14:01:59 +0000

    Runtime: 6 ms (13.97%), Memory: 45.8 MB (7.52%) - Gitcode

--- 268-missing-number/268. Missing Number.java
+++ 268-missing-number/268. Missing Number.java
@@ -1,15 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-
-        for(int number : nums){
-            set.add(number);
+        // Sort then return when nums[n] != n
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i] != i) return i;
         }
-
-        for(int i=0;i<nums.length;i++){
-            if(!set.contains(i)) return i;
-        }
-
+        // If none are not equal then we're missing the next number on
         return nums.length;
     }
 }
