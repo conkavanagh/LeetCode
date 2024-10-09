00bfe0c055e8e5407afdc3fe286e7ff23e595d6f
Conor Kavanagh <116828308+conkavanagh@users.noreply.github.com>
2024-10-09 13:15:02 +0100

    Runtime: 0 ms (100.00%), Memory: 41.4 MB (64.62%) - Gitcode

--- 921-minimum-add-to-make-parentheses-valid/921. Minimum Add to Make Parentheses Valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921. Minimum Add to Make Parentheses Valid.java
@@ -1,16 +1,16 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> stack = new Stack<>();
         int toChange = 0;
+        int open = 0;
         for(int i=0; i<s.length();i++){
-            if(s.charAt(i) == '(') stack.push(')');
+            if(s.charAt(i) == '(') open++;
             else{
-                if(!stack.isEmpty()) stack.pop();
+                if(open>0) open--;
                 else{
                     toChange++;
                 }
             }
         }
-        return toChange+=stack.size();
+        return toChange+=open;
     }
 }
