c5bed99b144e7e7330f5dc71eb4a71dc66c0758c
Conor Kavanagh <116828308+conkavanagh@users.noreply.github.com>
2024-10-09 13:10:55 +0100

    Runtime: 1 ms (58.43%), Memory: 41.4 MB (64.62%) - Gitcode

---
+++ 921-minimum-add-to-make-parentheses-valid/921. Minimum Add to Make Parentheses Valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int toChange = 0;
+        for(int i=0; i<s.length();i++){
+            if(s.charAt(i) == '(') stack.push(')');
+            else{
+                if(!stack.isEmpty()) stack.pop();
+                else{
+                    toChange++;
+                }
+            }
+        }
+        while(!stack.isEmpty()){
+            stack.pop();
+            toChange++;
+        }
+        return toChange;
+    }
+}
