294a65ad617849a0e29d92b93e6ae044d2e53a4f
Conor Kavanagh <116828308+conkavanagh@users.noreply.github.com>
2024-10-26 16:23:07 +0100

    Runtime: 42 ms (75.49%), Memory: 55 MB (83.99%) - Gitcode

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233. Remove Sub-Folders from the Filesystem.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+
+        List<String> res = new ArrayList<>();
+        res.add(folder[0]);
+
+        for(int i=1; i<folder.length; i++){
+            String lastFolder = res.get(res.size()-1) + "/";
+            if(!folder[i].startsWith(lastFolder)) res.add(folder[i]);
+        }
+
+        return res;
+    }
+}
