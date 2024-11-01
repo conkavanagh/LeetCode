9f1e7456f157b24c85d4fa4e24f0c4804869d877
Conor Kavanagh <116828308+conkavanagh@users.noreply.github.com>
2024-11-01 13:25:41 +0000

    Runtime: 40 ms (41.86%), Memory: 45.8 MB (21.25%) - Gitcode

---
+++ 1957-delete-characters-to-make-fancy-string/1957. Delete Characters to Make Fancy String.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder res = new StringBuilder();
+        if(s.length() < 2) return s;
+        int index = 2;
+        res.append(s.charAt(0));
+        res.append(s.charAt(1));
+        while(index<s.length()){
+            if(s.charAt(index) == s.charAt(index-1) && s.charAt(index) == s.charAt(index-2)){
+            }
+            else{
+                res.append(s.charAt(index));
+            }
+            index++;
+        }
+        return res.toString();
+    }
+}
