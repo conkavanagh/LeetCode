c71a7d85d5eb68f8d0ea6a891a134cb576e85f58
Conor Kavanagh <116828308+conkavanagh@users.noreply.github.com>
2024-11-03 13:52:53 +0000

    Runtime: 0 ms (100.00%), Memory: 41.4 MB (51.17%) - Gitcode

--- 796-rotate-string/796. Rotate String.java
+++ 796-rotate-string/796. Rotate String.java
@@ -1,9 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         if(s.length() != goal.length()) return false;
-
-        String full = s+s;
-        if(full.contains(goal)) return true;
-        return false;
+        return (s+s).contains(goal);
     }
 }
