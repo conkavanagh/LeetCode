bc3a1975a95fe48830def3b1879343d17fc4f1fa
Conor Kavanagh <116828308+conkavanagh@users.noreply.github.com>
2024-11-08 10:22:04 +0000

    Runtime: 2 ms (100.00%), Memory: 59.1 MB (37.22%) - Gitcode

--- 1829-maximum-xor-for-each-query/1829. Maximum XOR for Each Query.java
+++ 1829-maximum-xor-for-each-query/1829. Maximum XOR for Each Query.java
@@ -2,13 +2,14 @@ class Solution {
     public int[] getMaximumXor(int[] nums, int maximumBit) {
         int xorr = nums[0];
         int maxXor = (1<<maximumBit)-1;
+        int n = nums.length;
 
-        for(int i=1; i<nums.length; i++) xorr ^= nums[i];
+        for(int i=1; i<n; i++) xorr ^= nums[i];
 
-        int[] ans = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
             ans[i] = xorr ^ maxXor;
-            xorr ^= nums[nums.length-1-i];
+            xorr ^= nums[n-1-i];
         }
         return ans;
     }
